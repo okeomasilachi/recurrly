c9e9211959591d729e4ac07358ade72ad121db82
okeomasilachi <okeomasilachi@gmail.com>
2026-06-06 03:29:31 +0100

    feat: add custom fonts and update layout to handle font loading

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,13 @@
 import "@/global.css";
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
 import Sav from "@/ui/sav";
+import { Link } from "expo-router";
+import { Text } from "react-native";
 
 export default function App() {
   return (
     <Sav>
-      <Text className="text-xl font-bold text-success">
-        Welcome to Nativewind!
-      </Text>
+      <Text className="text-7xl font-sans-extrabold">Home</Text>
+      <Text className="text-7xl font-bold">Home</Text>
       <Link href="/onboarding" className="mt-4 rounded bg-primary text-white p-4">Go To Onboarding</Link>
       <Link href="/(auth)/sign-in" className="mt-4 rounded bg-primary text-white p-4">Go To Onboarding</Link>
       <Link href="/(auth)/sign-up" className="mt-4 rounded bg-primary text-white p-4">Go To Onboarding</Link>
